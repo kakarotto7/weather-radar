ffc9619dd5f18fb00e5c98e10ad6c39d65bd983a
kakarotto7 <67836221+kakarotto7@users.noreply.github.com>
2021-07-03 16:24:29 +0530

    Add files via upload

---
+++ src/components/app.js
@@ -0,0 +1,192 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import debounce from 'lodash-es/debounce';
+
+
+import About from './about';
+import Footer from './footer';
+import WeatherCard from './weather-card';
+import NavBar from './navbar';
+import Loading from './loading';
+import Search from './search';
+
+const apiUrl = process.env.REACT_APP_API_URL;
+const apiKey = process.env.REACT_APP_API_KEY;
+
+const App = () => {
+  const searchTimeout = 1000;
+  const [location, setLocation] = React.useState('Ajmer');
+  const [error, setError] = React.useState(null);
+  const [forecast, setForecast] = React.useState([]);
+  const [weather, setWeather] = React.useState({});
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = React.useState('');
+  const [isSearching, setIsSearching] = React.useState(false);
+  const [units, setUnits] = React.useState('metric');
+
+  const debounceSearch = React.useMemo(
+    () =>
+      debounce((searchTerm) => {
+        setDebouncedSearchTerm(searchTerm);
+      }, searchTimeout),
+    [],
+  );
+
+  const handleLocationChange = (event) => {
+    if (event.target.value) {
+      setIsSearching(true);
+    }
+    debounceSearch(event.target.value);
+  };
+
+  const handleUnitsChange = (newUnits) => {
+    setUnits(newUnits);
+  };
+
+  React.useEffect(() => {
+    if (debouncedSearchTerm) {
+      setLocation(debouncedSearchTerm);
+    }
+  }, [debouncedSearchTerm, isSearching]);
+
+  React.useEffect(() => {
+    async function getWeather() {
+      setError(null);
+      setIsSearching(false);
+
+      try {
+        const weather = await fetchWeather(location, units);
+        setWeather(weather);
+      } catch (err) {
+        setError(err);
+      }
+    }
+
+    getWeather();
+  }, [location, units]);
+
+  React.useEffect(() => {
+    async function getForecast() {
+      setError(null);
+      setIsSearching(false);
+
+      try {
+        const forecast = await fetchForecast(location, units);
+        setForecast(forecast);
+      } catch (err) {
+        setError(err);
+      }
+    }
+
+    getForecast();
+  }, [location, units]);
+
+  return (
+    <>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact path="/">
+            {(weather && Object.keys(weather).length) ||
+            (forecast && Object.keys(forecast).length) ? (
+              <main>
+                <div >
+                  <Search
+                    location={location}
+                    error={error}
+                    isSearching={isSearching}
+                    onLocationChange={handleLocationChange}
+                  />
+                  <WeatherCard
+                    forecast={forecast}
+                    weather={weather}
+                    units={units}
+                    onUnitsChange={handleUnitsChange}
+                  />
+                  <Footer />
+                </div>
+              </main>
+            ) : (
+              <Loading />
+            )}
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </Router>
+    </>
+  );
+};
+
+async function fetchForecast(location, units) {
+  const response = await window.fetch(
+    `${apiUrl}/forecast/?q=${location}&units=${units}&APPID=${apiKey}`,
+  );
+  const forecast = await response.json();
+  if (response.ok) {
+    if (Object.entries(forecast).length) {
+      return forecast.list
+        .filter((f) => f.dt_txt.match(/09:00:00/))
+        .map(mapDataToWeatherInterface);
+    }
+  } else {
+    const error = new Error(`No results for "${location}"`);
+    return Promise.reject(error);
+  }
+}
+
+async function fetchWeather(location, units) {
+  const response = await window.fetch(
+    `${apiUrl}/weather/?q=${location}&units=${units}&APPID=${apiKey}`,
+  );
+  const weather = await response.json();
+  if (response.ok) {
+    if (Object.entries(weather).length) {
+      return mapDataToWeatherInterface(weather);
+    }
+  } else {
+    const error = new Error(`No results for "${location}"`);
+    return Promise.reject(error);
+  }
+}
+
+function mapDataToWeatherInterface(data) {
+  const mapped = {
+    location: data.name,
+    condition: data.cod,
+    country: data.sys.country,
+    date: data.dt * 1000, // convert from seconds to milliseconds
+    description: data.weather[0].description,
+    feels_like: Math.round(data.main.feels_like),
+    humidity: data.main.humidity,
+    icon_id: data.weather[0].id,
+    sunrise: data.sys.sunrise * 1000, // convert from seconds to milliseconds
+    sunset: data.sys.sunset * 1000, // convert from seconds to milliseconds
+    temperature: Math.round(data.main.temp),
+    timezone: data.timezone / 3600, // convert from seconds to hours
+    wind_speed: Math.round(data.wind.speed * 3.6), // convert from m/s to km/h
+  };
+
+  // Add extra properties for the five day forecast: dt_txt, icon, min, max
+  if (data.dt_txt) {
+    mapped.dt_txt = data.dt_txt;
+  }
+
+  if (data.weather[0].icon) {
+    mapped.icon = data.weather[0].icon;
+  }
+
+  if (data.main.temp_min && data.main.temp_max) {
+    mapped.max = Math.round(data.main.temp_max);
+    mapped.min = Math.round(data.main.temp_min);
+  }
+
+  // remove undefined fields
+  Object.entries(mapped).map(
+    ([key, value]) => value === undefined && delete mapped[key],
+  );
+
+  return mapped;
+}
+
+export default App;
---
+++ src/components/footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="items-center mt-28">
+      <p className="text-center">
+        <span className="text-gray-600">
+          Open source by{' '}
+          <a className="text-indigo-600" href="https://github.com/kakarotto7/weather-radar">Rajat Goyal - </a>
+          
+          <span className="ml-4">Copyright &copy; 2021</span>
+        </span>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
