e2cb5ce43a98c37ad13f562d06df6987ce6dfc72
kakarotto7 <67836221+kakarotto7@users.noreply.github.com>
2021-07-03 16:25:15 +0530

    Add files via upload

---
+++ src/app-test-utils.js
@@ -0,0 +1,31 @@
+//import React from 'react';
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { createMemoryHistory } from 'history';
+import { BrowserRouter, Router } from 'react-router-dom';
+
+const renderWithRouter = (ui, { route = '/' } = {}) => {
+  window.history.pushState({}, 'Test page', route);
+
+  return render(ui, { wrapper: BrowserRouter });
+};
+
+const waitForLoadingToFinish = () =>
+  waitForElementToBeRemoved(() => [...screen.queryAllByRole(/progressbar/i)], {
+    timeout: 4000,
+  });
+
+export * from '@testing-library/react';
+export {
+  render,
+  screen,
+  userEvent,
+  renderWithRouter,
+  createMemoryHistory,
+  waitForLoadingToFinish,
+  Router,
+};
